52cd93799b450e6e89def79860eff7d8d5ca70de
julia <hogan.julia@gmail.com>
2022-06-08 14:22:34 -0400

    Add in-browser timing test

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,15 +1,32 @@
 import React from 'react';
 
 import { latLng, LatLngTuple } from 'leaflet';
+import distance from '@turf/distance';
+import { point } from '@turf/helpers';
 
 import '@trussworks/react-uswds/lib/uswds.css'
 import '@trussworks/react-uswds/lib/index.css'
-import { Header, GridContainer, Grid, Title, Alert } from '@trussworks/react-uswds'
+import { Header, GridContainer, Grid, Title, Alert, Button } from '@trussworks/react-uswds'
 
 import './App.css';
 
 const CO_CDHS: LatLngTuple= [39.741367, -104.985207];
 const CO_CAPITOL: LatLngTuple = [39.739292, -104.984886];
+
+const doTimingTest = () => {
+  const ref: LatLngTuple = [parseFloat((Math.random() * 180).toFixed(6)), parseFloat((Math.random() * 180).toFixed(6))]
+  const latlongs: LatLngTuple[] = Array.from(Array(40000)).map(x => [parseFloat((Math.random() * 180).toFixed(6)), parseFloat((Math.random() * 180).toFixed(6))])
+
+  const leafletStart = (new Date()).getTime()
+  latlongs.forEach(l => latLng(ref).distanceTo(latLng(l)));
+  const leafletFinish = (new Date()).getTime()
+
+  const turfStart = (new Date()).getTime();
+  latlongs.forEach(l => distance(point(ref), point(l)));
+  const turfFinish = (new Date()).getTime();
+  console.log("leaflet time (ms)", leafletFinish- leafletStart);
+  console.log("turf time (ms)", turfFinish - turfStart);
+}
 
 function App() {
   return (
@@ -21,6 +38,8 @@ function App() {
         <Alert headingLevel="h2" type='info' heading="Distance calculated in memory!">
           The distance between the CO Department of Human Services and the CO Capitol building is {latLng(CO_CDHS).distanceTo(CO_CAPITOL)} meters
         </Alert>
+
+        <Button onClick={doTimingTest} type="button">Do timing test</Button>
 
         <GridContainer>
           <Title>Responsive columns that stack into rows on small screens</Title>
